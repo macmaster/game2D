6306faa9e7318498ac29070f7915d135faad168d
ronny <ronnymacmaster@gmail.com>
2017-02-19 07:13:20 -0600

    refactor naming

--- src/com/ronny/driver/MouseLookDriver.java
+++ src/com/ronny/driver/MouseLookDriver.java
@@ -1,35 +1,30 @@
 package com.ronny.driver;
 
-import java.awt.Color;
+import java.awt.AWTException;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Point;
-import java.awt.RenderingHints;
+import java.awt.Robot;
+import java.awt.Toolkit;
 import java.awt.Window;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseWheelListener;
-import java.util.LinkedList;
+
+import javax.swing.SwingUtilities;
 
 /** KeyboardDriver
  * @author ronny <br>
  *
  * Capture and display AWT Events from keyboard and elswhere. <br>
  */
-public class MouseLookDriver extends Core implements KeyListener, MouseListener, MouseMotionListener, MouseWheelListener {
+public class MouseLookDriver extends Core implements KeyListener, MouseMotionListener {
 	
-	public LinkedList<Point> trail;
-	public boolean trailing;
-	private final int capacity = 100;
-	
-	// background color scrolling.
-	private int colorIndex;
-	public final Color[] colors = { new Color(140, 217, 179), new Color(128, 191, 255), new Color(217, 140, 140),
-			new Color(217, 179, 255), new Color(163, 163, 194) };
-	private Color BACKGROUND_COLOR = Core.BACKGROUND_COLOR;
+	private Robot robot;
+	private Image backgroundImage;
+	private Point mouseLocation, centerLocation, imageLocation;
+	private boolean recenteringMode, relativeMode;
 	
 	public static void main(String[] args) {
 		Core driver = new MouseLookDriver();
@@ -44,47 +39,64 @@ public class MouseLookDriver extends Core implements KeyListener, MouseListener,
 		Window window = screen.getFullScreenWindow();
 		window.setFocusTraversalKeysEnabled(false);
 		window.addKeyListener(this);
-		window.addMouseListener(this);
 		window.addMouseMotionListener(this);
-		window.addMouseWheelListener(this);
 		
-		// enqueue welcome message.
-		this.colorIndex = 0;
-		this.trailing = true;
-		this.trail = new LinkedList<Point>();
+		// set invisible cursor.
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		window.setCursor(kit.createCustomCursor(kit.getImage(""), new Point(0, 0), "invisible"));
+		
+		try { // create the robot.
+			this.robot = new Robot();
+			//this.recenterMouse();
+			//this.mouseLocation.setLocation(centerLocation);
+		} catch (AWTException e) {
+			e.printStackTrace();
+			screen.exitScreenMode();
+			System.err.println("Couldn't create robot. exiting...");
+			System.exit(1); // error
+		}
+		
+		// points and mouse modes.
+		this.relativeMode = true;
+		this.recenteringMode = false;
+		this.mouseLocation = new Point();
+		this.centerLocation = new Point();
+		this.imageLocation = new Point();
+		this.backgroundImage = loadImage("/nyc.jpg");
+		
+	}
+	
+	/**
+	 * Use the robot class to recenter the mouse <br>
+	 * Available on most modern platforms... <br>
+	 */
+	private synchronized void recenterMouse() {
+		Window window = screen.getFullScreenWindow();
+		if (window.isShowing() && robot != null) {
+			recenteringMode = true;
+			centerLocation.x = window.getWidth() / 2;
+			centerLocation.y = window.getHeight() / 2;
+			SwingUtilities.convertPointToScreen(centerLocation, window);
+			robot.mouseMove(centerLocation.x, centerLocation.y);
+		}
 	}
 	
 	@Override
 	public void draw(Graphics2D g) {
-		// // print message queue.
-		// Window window = screen.getFullScreenWindow();
-		//
-		// int red = (int) (Math.random() * 255);
-		// int blue = (int) (Math.random() * 255);
-		// int green = (int) (Math.random() * 255);
-		// Color RANDOM_COLOR = new Color(red, blue, green);
+		// verify / clip / saturate image location
+		int width = screen.getWidth(), height = screen.getHeight();
+		int imageWidth = backgroundImage.getWidth(null), imageHeight = backgroundImage.getHeight(null);
+		if (imageLocation.x > 0) imageLocation.x = 0;
+		if (imageLocation.y > 0) imageLocation.y = 0;
+		if (imageLocation.x < width - imageWidth) imageLocation.x = width - imageWidth;
+		if (imageLocation.y < height - imageHeight) imageLocation.y = height - imageHeight;
 		
-		// background color.
-		g.setColor(BACKGROUND_COLOR);
-		g.fillRect(0, 0, screen.getWidth(), screen.getHeight());
+		//g.drawImage(backgroundImage, imageLocation.x, imageLocation.y, null);
+		int x = imageLocation.x, y = imageLocation.y;
+		g.drawImage(backgroundImage, x, y, imageWidth, imageHeight, null);
 		
-		// exit message.
 		g.setColor(FOREGROUND_COLOR);
-		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		g.drawString("RainbowSnake: Press Escape to exit.", 5, FONT_SIZE);
-		
-		// print trail
-		for (int i = 0; i < trail.size() && trailing; i++) {
-			Point p = trail.get(i);
-			//g.drawString("[" + (n - i) + "]", p.x, p.y);
-			
-			int red = (int) (Math.random() * 255);
-			int blue = (int) (Math.random() * 255);
-			int green = (int) (Math.random() * 255);
-			Color RANDOM_COLOR = new Color(red, blue, green);
-			g.setColor(RANDOM_COLOR);
-			g.drawString("[<" + (i + 1) + ">]", p.x, p.y);
-		}
+		g.drawString("Press Escape to exit.", 5, FONT_SIZE);
 	}
 	
 	public void keyPressed(KeyEvent event) {
@@ -92,6 +104,8 @@ public class MouseLookDriver extends Core implements KeyListener, MouseListener,
 		int keyCode = event.getKeyCode();
 		if (keyCode == KeyEvent.VK_ESCAPE) {
 			stop();
+		} else if (keyCode == KeyEvent.VK_SPACE) {
+			relativeMode = !relativeMode;
 		}
 		event.consume();
 	}
@@ -104,53 +118,27 @@ public class MouseLookDriver extends Core implements KeyListener, MouseListener,
 		// TODO Auto-generated method stub
 	}
 	
-	public void mouseWheelMoved(MouseWheelEvent event) {
-		// TODO Auto-generated method stub
-		colorIndex += (event.getWheelRotation());
-		colorIndex %= colors.length;
-		colorIndex = (colorIndex < 0) ? colorIndex + colors.length : colorIndex;
-		BACKGROUND_COLOR = colors[colorIndex];
-		screen.getFullScreenWindow().setBackground(BACKGROUND_COLOR);
-	}
-	
 	public void mouseDragged(MouseEvent event) {
 		// TODO Auto-generated method stub
-		
+		mouseMoved(event);
 	}
 	
 	public void mouseMoved(MouseEvent event) {
 		// TODO Auto-generated method stub
-		mouseReleased(event);
-		
-	}
-	
-	public void mouseClicked(MouseEvent event) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public void mouseEntered(MouseEvent event) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public void mouseExited(MouseEvent event) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public void mousePressed(MouseEvent event) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public void mouseReleased(MouseEvent event) {
-		// TODO Auto-generated method stub
-		Point p = new Point(event.getX(), event.getY());
-		trail.addFirst(p);
-		if (trail.size() > capacity) {
-			trail.removeLast();
+		if (recenteringMode && centerLocation.equals(event.getPoint())) {
+			recenteringMode = false;
+		} else {
+			// calculate mouse movement.
+			int dx = event.getX() - mouseLocation.x;
+			int dy = event.getY() - mouseLocation.y;
+			imageLocation.x += dx;
+			imageLocation.y += dy;
+			if (relativeMode) {
+				recenterMouse();
+			}
 		}
+		
+		// update mouse location.
+		mouseLocation.setLocation(event.getPoint());
 	}
-	
 }
